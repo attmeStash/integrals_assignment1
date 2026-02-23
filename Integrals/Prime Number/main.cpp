6af50124e0573dbdd3133a06bbf503423fcd84c3
arttme <artemijs.stasans@gmail.com>
2026-02-23 21:58:07 +0200

    Add range prime listing and optimized check
    Add user input for minimum and maximum values with validation and a loop that lists primes in the range (brute-force divisor count). Introduce optimizedPrime(x) implementing a 6k±1 style check (testing i and i+2 up to sqrt(x)) and call it from main after prime(userInput()). Retain original naive approach comment for the range loop.

--- Integrals/Prime Number/main.cpp
+++ Integrals/Prime Number/main.cpp
@@ -41,10 +41,81 @@ void prime(unsigned int x)
 		std::cout << x << " is not prime" << std::endl;
 
 	}
+	unsigned int min;
+	unsigned int max;
+	std::cout << "Input a minimum number" << std::endl;
+	std::cin >> min;
+	while (std::cin.fail())  //checking for invalid input of the first number
+	{
+		std::cerr << "Error occured \n" << std::endl;
+
+		std::cin.clear();
+		std::cin.ignore(10000, '\n');
+
+		std::cout << "Enter a correct number" << std::endl;
+		std::cin >> min;
+	}
+
+
+	std::cout << "Input a maximum number" << std::endl;
+	std::cin >> max;
+	while (std::cin.fail())  //checking for invalid input of the first number
+	{
+		std::cerr << "Error occured \n" << std::endl;
+
+		std::cin.clear();
+		std::cin.ignore(10000, '\n');
+
+		std::cout << "Enter a correct number" << std::endl;
+		std::cin >> max;
+	}
+
+
+		for (int i = min; i < max; i++)
+		{
+			int primeCounter = 0;
+
+			for (int j = 1; j <= i; j++) // i know that we should check only up to sqrt of i, however this way it was just easier for me to complete the task
+			{
+				if (i % j == 0)
+				{
+					primeCounter++;
+				}
+				
+			}
+			if (primeCounter == 2)
+			{
+				std::cout << i << std::endl;
+			}
+
+		}
+	
+}
+
+void optimizedPrime(int x)
+{
+	bool isPrime = true;
+
+	for (int i = 5; i*i <= x; i+=6)
+	{
+		if ((x%i==0)||(x % (i + 2) == 0))
+		{
+			isPrime = false;			
+			break;
+		}
+	
+
+	}
+	if (isPrime)
+		std::cout << x << " is prime" << std::endl;
+	else
+		std::cout << x << " is not prime" << std::endl;
+
 }
 
 
 int main()
 {
 	prime(userInput());
+	optimizedPrime(userInput());
 }
