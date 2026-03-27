0f6aa2b6860472736924da876fc6172aa4010b83
arttme <artemijs.stasans@gmail.com>
2026-03-27 20:40:00 +0200

    Add CHATGPT_tasks_templates Visual Studio project
    Introduce a new Visual Studio C++ project 'CHATGPT_tasks_templates' with project (.vcxproj) and filter files, and a sample main.cpp. The project is configured for Win32 and x64 Debug/Release, uses PlatformToolset v145 and C++20 (stdcpp20). main.cpp provides simple template functions (minValue and swapValues) and a demo usage of swapValues. Update the Integrals.slnx to include the new project.

---
+++ Integrals/CHATGPT_tasks_templates/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+
+template <typename T>
+int minValue(T a, T b)
+{
+	return a < b ? a : b;
+}
+
+
+template <typename T>
+void swapValues(T& a, T& b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+
+
+int main()
+{
+	/*{
+		int input1, input2;
+
+		std::cin >> input1;
+		std::cin >> input2;
+
+
+		std::cout << minvalue(input1, input2) << std::endl;
+	}*/
+
+	{
+		int x = 5;
+		int y = 10;
+		swapValues(x, y);
+		std::cout << x << " " << y << std::endl;
+	}
+
+
+	return 0;
+}
